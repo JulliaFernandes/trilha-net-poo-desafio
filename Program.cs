241fbcd2646de5f118a45df5bb47edb83b59b04c
Jullia Fernandes <julliafernandes41@hotmail.com>
2023-11-09 04:25:00 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace Trilha_net_poo_desafio_main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Smartphone Nokia");
+            Nokia nk = new Nokia(numero: "123456789", modelo: "Nokia", imei:"11212121", memoria: 128);
+            nk.Ligar();
+            nk.InstalarAplicativo("Tiktok");
+            nk.ReceberLigacao();
+
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("Smartphone Iphone");
+            Iphone ip = new Iphone(numero: "987654321", modelo: "Iphone 12 Pro Max", imei:"3433434343", memoria: 256);
+            nk.Ligar();
+            nk.InstalarAplicativo("Linkedin");
+            nk.ReceberLigacao();
+        }
+    }
+}
